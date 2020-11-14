260d35f80ca19565fc98579baddeec54c9b72057
Kostanios <kotov.const@mail.ru>
2020-11-14 18:54:49 +0300

    init: web pack ref

---
+++ src/js/postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('mqpacker'),
+        require('cssnano')(
+            {
+                preset:[
+                    'default',{
+                        discardComments:{
+                            removeAll: true,
+                        }
+                    }
+                ]
+            }
+        )
+    ]
+    
+}
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports={
+    entry:{
+        app:'./src/index.js'
+    },
+    output:{
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module:{
+        rules :[
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                     {
+                        loader:'css-loader',
+                        options:{ sourceMap: true}
+                     },
+                     {
+                        loader:'postcss-loader',
+                        options:{ sourceMap: true, config: {path:'src/js/postcss.config.js'}}
+                     },
+                    ],
+            },
+
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                     {
+                        loader:'css-loader',
+                        options:{ sourceMap: true}
+                     },
+                     {
+                        loader:'postcss-loader',
+                        options:{ sourceMap: true, config: {path:'src/js/postcss.config.js'}}
+                     },
+                     {
+                        loader:'sass-loader',
+                        options:{ sourceMap: true}
+                     },
+                    ],
+            }
+        ]
+
+ 
+    },
+    devServer:{
+        overlay: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: '[name].css',
+          chunkFilename: '[id].css',
+        }),
+      ],
+      
+}
